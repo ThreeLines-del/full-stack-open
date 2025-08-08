a37502e415de256f012e246b9063859b20eb2c9f
Three Lines <threedqj@gmail.com>
2025-08-08 23:33:24 +0000

    Blog backend with tests

---
+++ part4/blogbackend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blog");
+
+const app = express();
+
+const mongoUrl = config.MONGODB_URI;
+
+logger.info("connecting to", mongoUrl);
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info("connected to database");
+  })
+  .catch(() => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
